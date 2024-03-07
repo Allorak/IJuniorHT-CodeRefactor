8f5a18febce7cd29126d7d4b48bd0fec96e3ceba
Моисеев Михаил <allorakspam@gmail.com>
2024-03-07 03:40:21 +0300

    Fixed navigator logic

--- RouteNavigator.cs
+++ RouteNavigator.cs
@@ -3,21 +3,15 @@ using UnityEngine;
 public class RouteNavigator : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private Transform _wayPointsHolder;
+    [SerializeField] private WayPoint[] _wayPoints;
     
-    private Transform[] _wayPoints;
     private int _currentWayPointIndex = 0;
-    
-    private void Start()
-    {
-        _wayPoints = _wayPointsHolder.GetComponentsInChildren<Transform>();
-    }
-    
+
     private void Update()
     {
         var currentWaypointPosition = _wayPoints[_currentWayPointIndex].position;
         var step = _speed * Time.deltaTime;
-        transform.position   =  Vector3.MoveTowards(transform.position , currentWaypointPosition, step);
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypointPosition, step);
 
         if (transform.position == currentWaypointPosition)
             _currentWayPointIndex = ++_currentWayPointIndex % _wayPoints.Length;
